3f95d3c6d68ce134ecc80ac12671fe21e1f57173
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2014-01-18 23:20:47 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Sarah Kankiewicz</title>
+    <link href="/img/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+
+    <!-- META -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- STYLES / FONTS -->
+    <link href="/css/bootstrap.min.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="/css/font-awesome.min.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="/css/style.css" media="screen" rel="stylesheet" type="text/css">
+
+    <!-- SCRIPTS -->
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+    <div class="site-title">
+        Sarah Kankiewicz
+    </div>
+
+</body>
+
+</html>
