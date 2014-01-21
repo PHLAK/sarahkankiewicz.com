eb75ec053ff2be15f6f883aea29da066dd089ed1
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2014-01-20 18:08:26 -0700

    Initial design and layout implimented

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <!-- STYLES / FONTS -->
     <link href="/css/bootstrap.min.css" media="screen" rel="stylesheet" type="text/css">
     <link href="/css/font-awesome.min.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="//fonts.googleapis.com/css?family=Arvo" media="screen" rel="stylesheet" type="text/css">
     <link href="/css/style.css" media="screen" rel="stylesheet" type="text/css">
 
     <!-- SCRIPTS -->
@@ -23,8 +24,127 @@
 
 <body>
 
-    <div class="site-title">
-        Sarah Kankiewicz
+    <div class="container">
+
+        <div class="page-wrapper">
+
+            <div class="site-title">
+                <div class="row">
+
+                    <div class="col-md-4 text-center">
+                        <img src="//placehold.it/120" alt="Sarah Kankiewicz" class="title-img img-circle">
+                    </div>
+
+                    <div class="col-md-8 text-center">
+
+                        <h1 class="title-text">
+                            Sarah Kankiewicz <br>
+                            <small>Certified Lactation Educator</small>
+                        </h1>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="row">
+
+                <div class="navigation-wrapper col-md-4">
+
+                    <ul class="site-navigation">
+
+                        <li>
+                            <a href="#" class="clearfix">
+
+                                <span class="nav-icon about">
+                                    <i class="fa fa-star fa-lg fa-fw"></i>
+                                </span>
+
+                                <span class="nav-text">
+                                    About Sarah
+                                </span>
+
+                                <span class="nav-caret">
+                                     <i class="fa fa-angle-right fa-fw"></i>
+                                </span>
+
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="clearfix">
+
+                                <span class="nav-icon email">
+                                    <i class="fa fa-envelope fa-lg fa-fw"></i>
+                                </span>
+
+                                <span class="nav-text">
+                                    Email Sarah
+                                </span>
+
+                                <span class="nav-caret">
+                                     <i class="fa fa-angle-right fa-fw"></i>
+                                </span>
+
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="clearfix">
+
+                                <span class="nav-icon facebook">
+                                    <i class="fa fa-facebook fa-lg fa-fw"></i>
+                                </span>
+
+                                <span class="nav-text">
+                                    Follow Sarah
+                                </span>
+
+                                <span class="nav-caret">
+                                     <i class="fa fa-angle-right fa-fw"></i>
+                                </span>
+
+                            </a>
+                        </li>
+
+                    </ul>
+
+                </div>
+
+                <div class="content-wrapper col-md-8">
+
+                    <h2 class="section-title">Section Title</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In commodo sodales
+                    dignissim. Ut quis euismod nibh. In in ante nec eros porttitor fringilla ut qin
+                    tellus. Praesent non urna sed orci feugiat volutpat. Etiam dapibus lorem auctor,
+                    sagittis mi ac, dignissim tortor. Suspendisse dolor nibh, malesuada quis magna
+                    vitae, lobortis feugiat eros. In tincidunt quis elit id euismod. Nullam
+                    condimentum sodales tortor, eu molestie enim eleifend at. Sed molestie tortor a
+                    felis egestas pellentesque. Donec ut gravida nisi, non blandit libero. Phasellus
+                    nec nibh nec urna adipiscing egestas. Morbi sed pharetra elit. Cras sodales erat
+                    vitae felis iaculis, ac tempus magna sollicitudin. Morbi condimentum mattis
+                    quam, nec cursus turpis imperdiet ac. Cras non porta augue, nec viverra
+                    tellus.</p>
+
+                    <p>In non vulputate nisi. Quisque interdum sit amet felis at interdum. Quisque a
+                    arcu ullamcorper, pretium purus nec, convallis nisl. Aliquam consequat nisl at
+                    sapien porttitor vestibulum sit amet ut lorem. Fusce convallis scelerisque orci
+                    id feugiat. Mauris suscipit orci ut felis bibendum, quis dictum metus posuere.
+                    Sed sit amet massa quis risus dapibus tristique.</p>
+
+                </div>
+
+            </div>
+
+            <div class="site-footer">
+
+                Copyright &copy; 2014 <a href="/">Sarah Kankiewicz</a> <br><br>
+                Design by <a href="https://www.chriskankiewicz.com">Chris Kankiewicz</a>
+
+            </div>
+
+        </div>
+
     </div>
 
 </body>
